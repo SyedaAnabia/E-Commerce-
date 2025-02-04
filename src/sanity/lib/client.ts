e048f0e0de0c2c86e119d0cbf7e54621b3655974
SyedaAnabia <samdrive30@gmail.com>
2025-02-05 03:14:42 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,7 +3,7 @@ import { createClient } from 'next-sanity';
 
 const client = createClient({
   projectId: "fbhn0r44",
-  dataset: "productions",
+  dataset: "production",
   apiVersion: '2025-01-18',
   useCdn: true, // Use `false` to always fetch fresh data
   token: process.env.SANITY_API_TOKEN, // Securely use the token
