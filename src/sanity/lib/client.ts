82012eda21e88a17228727f8362bbde8a29195cf
SyedaAnabia <samdrive30@gmail.com>
2025-02-05 03:13:44 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,7 +2,7 @@
 import { createClient } from 'next-sanity';
 
 const client = createClient({
-  projectId: "2srh4ekv",
+  projectId: "fbhn0r44",
   dataset: "productions",
   apiVersion: '2025-01-18',
   useCdn: true, // Use `false` to always fetch fresh data
